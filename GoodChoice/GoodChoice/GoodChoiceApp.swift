0c3b4f2c51dbe1b0d69dbfed375e649d256bcfb6
Ignat Kolesnikov <ignat160.kolesnikov@gmail.com>
2026-04-16 22:16:45 +0300

    #1: init

---
+++ GoodChoice/GoodChoice/GoodChoiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoodChoiceApp.swift
+//  GoodChoice
+//
+//  Created by Ignat Kolesnikov on 16.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct GoodChoiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
